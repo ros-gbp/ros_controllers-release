60439ee38fa15ff0896889f95128b84f8992706c
Bence Magyar <bence.magyar.robotics@gmail.com>
2018-04-27 10:02:43 +0100

    Rebase from 'upstream'

--- four_wheel_steering_controller/test/src/four_wheel_steering_4ws_cmd_test.cpp
+++ four_wheel_steering_controller/test/src/four_wheel_steering_4ws_cmd_test.cpp
@@ -5,10 +5,8 @@
 TEST_F(FourWheelSteeringControllerTest, testForward)
 {
   // wait for ROS
-  while(!isControllerAlive())
-  {
-    ros::Duration(0.1).sleep();
-  }
+  waitForController();
+
   // zero everything before test
   four_wheel_steering_msgs::FourWheelSteering cmd_vel;
   cmd_vel.speed = 0.0;
@@ -21,16 +19,20 @@ TEST_F(FourWheelSteeringControllerTest, testForward)
   // send a velocity command of 0.1 m/s
   cmd_vel.speed = 0.1;
   publish_4ws(cmd_vel);
-  // wait for 10s
-  ros::Duration(10.0).sleep();
+  // wait for Xs
+  double travel_time = 5.0;
+  ros::Duration(travel_time).sleep();
 
   nav_msgs::Odometry new_odom = getLastOdom();
+
+  const double actual_travel_time = (new_odom.header.stamp - old_odom.header.stamp).toSec();
+  const double expected_distance = cmd_vel.speed * actual_travel_time;
 
   // check if the robot traveled 1 meter in XY plane, changes in z should be ~~0
   const double dx = new_odom.pose.pose.position.x - old_odom.pose.pose.position.x;
   const double dy = new_odom.pose.pose.position.y - old_odom.pose.pose.position.y;
   const double dz = new_odom.pose.pose.position.z - old_odom.pose.pose.position.z;
-  EXPECT_NEAR(sqrt(dx*dx + dy*dy), 1.0, POSITION_TOLERANCE);
+  EXPECT_NEAR(sqrt(dx*dx + dy*dy), expected_distance, POSITION_TOLERANCE);
   EXPECT_LT(fabs(dz), EPS);
 
   // convert to rpy and test that way
@@ -53,10 +55,8 @@ TEST_F(FourWheelSteeringControllerTest, testForward)
 TEST_F(FourWheelSteeringControllerTest, testCrab)
 {
   // wait for ROS
-  while(!isControllerAlive())
-  {
-    ros::Duration(0.1).sleep();
-  }
+  waitForController();
+
   // zero everything before test
   four_wheel_steering_msgs::FourWheelSteering cmd_vel;
   cmd_vel.speed = 0.0;
@@ -71,19 +71,21 @@ TEST_F(FourWheelSteeringControllerTest, testCrab)
   cmd_vel.front_steering_angle = M_PI/8.0;
   cmd_vel.rear_steering_angle = cmd_vel.front_steering_angle;
   publish_4ws(cmd_vel);
-  // wait for 10s
-  double travel_time = 8.0;
+  // wait for Xs
+  double travel_time = 5.0;
   ros::Duration(travel_time).sleep();
 
   nav_msgs::Odometry new_odom = getLastOdom();
+
+  double actual_travel_time = (new_odom.header.stamp - old_odom.header.stamp).toSec();
 
   // check if the robot traveled 5 meter in XY plane, changes in z should be ~~0
   const double dx = new_odom.pose.pose.position.x - old_odom.pose.pose.position.x;
   const double dy = new_odom.pose.pose.position.y - old_odom.pose.pose.position.y;
   const double dz = new_odom.pose.pose.position.z - old_odom.pose.pose.position.z;
-  EXPECT_NEAR(sqrt(dx*dx + dy*dy), cmd_vel.speed*travel_time, POSITION_TOLERANCE);
-  EXPECT_NEAR(dx, cmd_vel.speed*travel_time*cos(cmd_vel.front_steering_angle), POSITION_TOLERANCE);
-  EXPECT_NEAR(dy, cmd_vel.speed*travel_time*sin(cmd_vel.front_steering_angle), POSITION_TOLERANCE);
+  EXPECT_NEAR(sqrt(dx*dx + dy*dy), cmd_vel.speed*actual_travel_time, POSITION_TOLERANCE);
+  EXPECT_NEAR(dx, cmd_vel.speed*actual_travel_time*cos(cmd_vel.front_steering_angle), POSITION_TOLERANCE);
+  EXPECT_NEAR(dy, cmd_vel.speed*actual_travel_time*sin(cmd_vel.front_steering_angle), POSITION_TOLERANCE);
   EXPECT_LT(fabs(dz), EPS);
 
   // convert to rpy and test that way
@@ -106,10 +108,8 @@ TEST_F(FourWheelSteeringControllerTest, testCrab)
 TEST_F(FourWheelSteeringControllerTest, testSymmetricTurn)
 {
   // wait for ROS
-  while(!isControllerAlive())
-  {
-    ros::Duration(0.1).sleep();
-  }
+  waitForController();
+
   // zero everything before test
   four_wheel_steering_msgs::FourWheelSteering cmd_vel;
   cmd_vel.speed = 0.0;
@@ -160,10 +160,8 @@ TEST_F(FourWheelSteeringControllerTest, testSymmetricTurn)
 TEST_F(FourWheelSteeringControllerTest, testNonSymmetricTurn)
 {
   // wait for ROS
-  while(!isControllerAlive())
-  {
-    ros::Duration(0.1).sleep();
-  }
+  waitForController();
+
   // zero everything before test
   four_wheel_steering_msgs::FourWheelSteering cmd_vel;
   cmd_vel.speed = 0.0;
@@ -215,10 +213,8 @@ TEST_F(FourWheelSteeringControllerTest, testNonSymmetricTurn)
 TEST_F(FourWheelSteeringControllerTest, testOdomFrame)
 {
   // wait for ROS
-  while(!isControllerAlive())
-  {
-    ros::Duration(0.1).sleep();
-  }
+  waitForController();
+
   // set up tf listener
   tf::TransformListener listener;
   ros::Duration(2.0).sleep();
